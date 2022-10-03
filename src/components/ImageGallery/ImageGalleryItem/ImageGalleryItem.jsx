6ab1bc8450fda45fe191894093429d3dffac1d23
Lisnyi <russelhobbbs@gmail.com>
2022-10-03 17:46:39 +0300

    homework done

--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
+import React,  { Component } from 'react'
 import { GalleryCard, CardImage } from './ImageGalleryItem.styled'
+import { Modal } from 'shared/components/Modal/Modal'
 
-export const ImageGalleryItem = ({images}) => {
+export class ImageGalleryItem extends Component {
+  state = {
+    modalOpen: false,
+    modalContent: {
+      largeImageURL: "",
+      tags: "",
+     }
+  }
+
+  openModal = (modalContent) => {
+    this.setState({
+      modalOpen: true,
+      modalContent,
+    })
+  }
+
+  onClose = () => {
+    this.setState({
+      modalOpen: false,
+      modalContent: {
+        largeImageURL: "",
+        tags: "",
+       }})
+  }
+
+  render() {
+    const { modalOpen, modalContent: {largeImageURL, tags} } = this.state
+    const { images } = this.props
+    const { onClose, openModal } = this 
     return (
-      images.map(({id, webformatURL, largeImageURL, tags}) => (
-        < GalleryCard key={id}>
-          <CardImage src={webformatURL} alt={tags} />
-        </ GalleryCard>
-      ))
+      <>
+        {images.map(({id, webformatURL, largeImageURL, tags}) => (
+          < GalleryCard key={id}>
+            <CardImage src={webformatURL} alt={tags} onClick={()=>openModal({largeImageURL, tags})}/>
+          </ GalleryCard>
+        ))}
+        {modalOpen && <Modal onClose={onClose}>
+                        <img src={largeImageURL} alt={tags}></img>
+                      </Modal>}
+      </>
     )
+  }
 }
+
---
+++ src/shared/components/Modal/Modal.jsx
@@ -0,0 +1,35 @@
+import { createPortal } from "react-dom"
+import React, { Component } from 'react'
+import { Overlay, ModalWindow } from "./Modal.styled"
+
+const modalRoot = document.getElementById("modal-root");
+
+export class Modal extends Component {
+
+    componentDidMount() {
+        document.addEventListener("keydown", this.closeModal);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener("keydown", this.closeModal)
+    }
+
+    closeModal = ({target, currentTarget, code}) => {
+        if (target === currentTarget || code === "Escape") {
+            this.props.onClose();
+        }
+    }
+
+  render() {
+    const {children} = this.props
+    const {closeModal} = this
+    return createPortal (
+        <Overlay onClick={closeModal}>
+            <ModalWindow>
+                {children}
+            </ModalWindow>
+        </Overlay>,
+        modalRoot
+    )
+  }
+}
