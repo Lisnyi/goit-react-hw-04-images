9ccaccb45965916f4e94ce4d969af44178d0076f
Lisnyi <russelhobbbs@gmail.com>
2022-10-07 23:48:28 +0300

    rework closeModal

--- src/shared/components/Modal/Modal.jsx
+++ src/shared/components/Modal/Modal.jsx
@@ -1,5 +1,5 @@
 import { createPortal } from "react-dom"
-import React, { useEffect } from 'react'
+import React, { useEffect, useCallback } from 'react'
 import PropTypes from 'prop-types'
 import { Overlay, ModalWindow } from "./Modal.styled"
 
@@ -7,18 +7,25 @@ const modalRoot = document.getElementById("modal-root");
 
 export const Modal = ({onClose, children}) => {
 
+    const closeModal = useCallback(
+        ({target, currentTarget, code}) => {
+            if (target === currentTarget || code === "Escape") {
+            onClose();
+        }}, [onClose]
+    )
+
     useEffect(() => {
         document.addEventListener("keydown", closeModal)
         return () => {
             document.removeEventListener("keydown", closeModal)
         }
-    }, [])
+    }, [closeModal])
 
-    function closeModal ({target, currentTarget, code}) {
-        if (target === currentTarget || code === "Escape") {
-            onClose();
-        }
-    }
+    // function closeModal ({target, currentTarget, code}) {
+    //     if (target === currentTarget || code === "Escape") {
+    //         onClose();
+    //     }
+    // }
 
     return createPortal (
             <Overlay onClick={closeModal}>
