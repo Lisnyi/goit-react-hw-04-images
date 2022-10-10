5b240861efc94145126261692bb40d08427da491
Lisnyi <russelhobbbs@gmail.com>
2022-10-10 16:57:12 +0300

    Update Modal.jsx

--- src/shared/components/Modal/Modal.jsx
+++ src/shared/components/Modal/Modal.jsx
@@ -21,12 +21,6 @@ export const Modal = ({onClose, children}) => {
         }
     }, [closeModal])
 
-    // function closeModal ({target, currentTarget, code}) {
-    //     if (target === currentTarget || code === "Escape") {
-    //         onClose();
-    //     }
-    // }
-
     return createPortal (
             <Overlay onClick={closeModal}>
                 <ModalWindow>
